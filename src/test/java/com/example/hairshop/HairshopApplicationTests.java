6c37b5d9bbe9b72989e42c99a5b2b2b1c76c1ee9
hyunwoo <khw4465@naver.com>
2024-03-21 10:16:36 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/hairshop/HairshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hairshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
